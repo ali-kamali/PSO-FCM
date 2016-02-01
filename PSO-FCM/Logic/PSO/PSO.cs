6a757695e91ca6946501cadda1970e7cf87648fa
ali-kamali <ali.kamali09@yahoo.com>
2016-02-01 11:33:25 +0330

    pso

---
+++ PSO-FCM/Logic/PSO/PSO.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+using PSO_FCM.Utility;
+
+namespace PSO_FCM.Logic.PSO
+{
+    public class Pso
+    {
+        public List<Particle> Particles { get; set; }
+        public Dim[] GloablBestPosition { get; set; }
+        public double GloablBestError { get; set; }
+        public int C { get; set; } //number of clusters
+        public int N { get; set; } //sample size
+        public double M { get; set; } //Fuzzy exponent
+        public int Num { get; set; }//Max Iteration
+        public int T { get; set; }//Time
+        public int D { get; set; }//Dimensions
+        public double W { get; set; }//Weight
+        public double C1 { get; set; }//Learning Factor
+        public double C2 { get; set; }//Learning Factor
+        public double[,] U { get; set; }//Matrix
+        public double Variance { get; set; } //Variance
+        public List<Data> Datas { get; set; }
+        public Pso(int c, int n, double m, double w, double c1, double c2, int num, int dim, List<Data> datas)
+        {
+            C = c;
+            N = n;
+            M = m;
+            Num = num;
+            T = 0;
+            D = dim;
+            Datas = datas;
+            W = w;
+            C1 = c1;
+            C2 = c2;
+            GloablBestError = double.MaxValue;
+            GloablBestPosition=new Dim[c];
+            Particles = new List<Particle>();
+            for (int a = 0; a < Num; a++)
+            {
+                
+                Particle p = new Particle
+                {
+                    U = new double[N, c],
+                    C = c,
+                    N = n,
+                    Position = new Dim[c],
+                    BestPosition = new Dim[c],
+                    Velocity = new Dim[c],
+                    Error = Double.MaxValue,
+                    BestError = Double.MaxValue,
+
+                };
+                for (int i = 0; i < c; i++)
+                {
+                    p.Position[i] = new Dim(D);
+                    p.Velocity[i] = new Dim(D);
+                    if (a == 0)
+                    {
+                        GloablBestPosition[i]= new Dim(D);
+                    }
+                    for (int j = 0; j < D; j++)
+                    {
+                        p.Position[i].Val[j] = GeneralCom.GetRandom();
+                        //p.Velocity[i].Val[j] = GeneralCom.GetRandom();
+                        if (a == 0)
+                        {
+                            GloablBestPosition[i].Val[j] = GeneralCom.GetRandom();
+                        }
+                    }
+                }
+                p.BestPosition = p.Position;
+                
+                p.CalcU(datas, M);
+                p.CalcV(W, C1, C2, GloablBestPosition);
+                Particles.Add(p);
+            }
+
+        }
+
+        public void Calc()
+        {
+            foreach (Particle particle in Particles)
+            {
+                particle.CalcError(Datas,M);
+                if (particle.BestError > particle.Error)
+                {
+                    particle.BestError = particle.Error;
+                    particle.BestPosition = particle.Position;
+                }
+            }
+            var bestp = Particles.OrderBy(p => p.BestError).FirstOrDefault();
+            if (bestp != null)
+            {
+                if (GloablBestError > bestp.BestError)
+                {
+                    GloablBestError = bestp.BestError;
+                    GloablBestPosition = bestp.BestPosition;
+                    U = bestp.U;
+                }    
+            }
+            double xbar = 0;
+            int cnt = 0;
+            foreach (Particle particle in Particles)
+            {
+                particle.CalcV(W,C1,C2,GloablBestPosition);
+                particle.UpdatePost();
+                xbar += particle.Error;
+                cnt++;
+            }
+            xbar = xbar/cnt;
+            Variance = Particles.Sum(particle => Math.Pow((particle.Error - xbar), 2))/cnt;
+        }
+    }
+}
---
+++ PSO-FCM/Logic/PSO/Particle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using PSO_FCM.Utility;
+
+namespace PSO_FCM.Logic.PSO
+{
+    public class Particle
+    {
+        public Dim[] Position { get; set; }
+        public double Error { get; set; }
+        public Dim[] Velocity { get; set; }
+        public Dim[] BestPosition { get; set; }
+        public double BestError{ get; set; }
+        public double[,] U { get; set; }//Matrix
+        public int C { get; set; } //number of clusters
+        public int N { get; set; } //sample size
+
+        public void CalcU(List<Data> datas, double m)
+        {
+            //File.WriteAllText("data.res", "");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < C; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < C; k++)
+                    {
+                        var a = GeneralCom.Euclideandistance(datas[i].DataDim, Position[j]);
+                        var b = GeneralCom.Euclideandistance(datas[i].DataDim, Position[k]);
+                        sum += Math.Pow((a / b), 2 / (m - 1));
+                    }
+                    U[i, j] = 1 / sum;
+                   // File.AppendAllText("data.res", U[i, j].ToString(CultureInfo.InvariantCulture) + ";");
+                }
+                //File.AppendAllText("data.res", "\t\n");
+            }
+        }
+
+        public void CalcV(double w, double c1, double c2, Dim[] globalPosition)
+        {
+            for (int c = 0; c< C; c++)
+            {
+                for (int d = 0; d < Velocity[c].Val.Length; d++)
+                {
+                    double y1 = GeneralCom.GetRandom(0, 1);
+                    double y2 = GeneralCom.GetRandom(0, 1);
+                    Velocity[c].Val[d] = w * Velocity[c].Val[d] + c1 * y1*(BestPosition[c].Val[d]-Position[c].Val[d])
+                        + c2 * y2 * (globalPosition[c].Val[d] - Position[c].Val[d]);
+                }
+            }
+        }
+
+        public void UpdatePost()
+        {
+            for (int c = 0; c < C; c++)
+            {
+                for (int d = 0; d < Velocity[c].Val.Length; d++)
+                {
+                    Position[c].Val[d] = Position[c].Val[d]+ Velocity[c].Val[d];
+                }
+            }
+        }
+
+        public void CalcError(List<Data> datas,double m)
+        {
+            double sigma = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < C; j++)
+                {
+                    sigma += Math.Pow(U[i, j], m)*
+                             Math.Pow(GeneralCom.Euclideandistance(datas[i].DataDim, Position[j]), 2);
+                }
+            }
+            Error = 1/(sigma+1);
+        }
+
+    }
+}
